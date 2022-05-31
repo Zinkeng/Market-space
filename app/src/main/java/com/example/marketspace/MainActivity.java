601deb679db31cf6f8087622488464af230861a2
Zinkeng <ajongafaczinkeng@gmail.com>
2022-05-31 14:15:08 +0100

    pages modified

--- app/src/main/java/com/example/marketspace/MainActivity.java
+++ app/src/main/java/com/example/marketspace/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_landingpage);
+        setContentView(R.layout.activity_signup);
 
     }
     public void ImageClicked(View v){
--- app/src/main/java/com/example/marketspace/Signup.java
+++ app/src/main/java/com/example/marketspace/Signup.java
@@ -2,13 +2,28 @@ package com.example.marketspace;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Signup extends AppCompatActivity {
 
+
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        button = (Button) findViewById(R.id.button3);
+
+    }
+
+    public void signUp(){
+        setContentView(R.layout.activity_verificationpage);
+        Intent intent = new Intent(this,verificationpage.class);
+        startActivity(intent);
+
     }
 }
